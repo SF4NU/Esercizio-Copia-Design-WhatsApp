8c5e0e588b1b3fb7bde4dfcf23122c4b3241b7ce
SF4NU <mariusmarius200121@gmail.com>
2024-02-07 22:05:17 +0100

    usato qualche estensione nuova sul codice

--- data/data.js
+++ data/data.js
@@ -3,91 +3,90 @@ export let people = [
     name: "John",
     message: "Hello, how are you?",
     hour: "10:00",
-    notifications: 3
-    },
+    notifications: 3,
+  },
   {
     name: "Alice",
     message: "I'm doing great, thanks!",
     hour: "11:30",
-    notifications: 1
-    },
+    notifications: 1,
+  },
   {
     name: "Mike",
     message: "What's up everyone?",
     hour: "13:45",
-    notifications: 5
-    },
+    notifications: 5,
+  },
   {
     name: "Sarah",
     message: "Has anyone seen my keys?",
     hour: "14:15",
-    notifications: 5
-    },
-    // Add more people as needed
+    notifications: 5,
+  },
   {
     name: "Emily",
     message: "I love this weather!",
     hour: "15:00",
-    notifications: 2
-    },
+    notifications: 2,
+  },
   {
     name: "David",
     message: "Who wants to grab lunch?",
     hour: "12:00",
-    notifications: 4
-    },
+    notifications: 4,
+  },
   {
     name: "Sophia",
     message: "Happy birthday, John!",
     hour: "9:00",
-    notifications: 1
-    },
+    notifications: 1,
+  },
   {
     name: "Ryan",
     message: "I'll be late for the meeting.",
     hour: "16:30",
-    notifications: 1
-    },
+    notifications: 1,
+  },
   {
     name: "Emma",
     message: "Can someone help me with this problem?",
     hour: "15:45",
-    notifications: 2
-    },
+    notifications: 2,
+  },
   {
     name: "Daniel",
     message: "Ready for the weekend!",
     hour: "17:00",
-    notifications: 3
-    },
+    notifications: 3,
+  },
   {
     name: "Olivia",
     message: "Does anyone have a pen I can borrow?",
     hour: "11:00",
-    notifications: 9
-    },
+    notifications: 9,
+  },
   {
     name: "Michael",
     message: "Just finished my project!",
     hour: "14:00",
-    notifications: 7
-    },
+    notifications: 7,
+  },
   {
     name: "Ella",
     message: "What's for dinner tonight?",
     hour: "18:15",
-    notifications: 2
-    },
+    notifications: 2,
+  },
   {
     name: "William",
     message: "Looking forward to the game tonight!",
     hour: "19:30",
-    notifications: 1
-    },
+    notifications: 1,
+  },
   {
     name: "Ava",
     message: "Anyone up for a movie night?",
     hour: "20:45",
-    notifications: 678
-    }
+    notifications: 678,
+  },
 ];
