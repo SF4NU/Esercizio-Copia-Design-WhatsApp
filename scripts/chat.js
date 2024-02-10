5ab31a98255db36f4ae58fcab4d893e44cf0a552
SF4NU <mariusmarius200121@gmail.com>
2024-02-10 17:38:13 +0100

    ora si ricevono risposte a caso ai messaggi, forse ultimo aggiornamento

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,19 +1,23 @@
-import { conversazione } from "../data/data.js";
-
+import { conversazione, risposte, people } from "../data/data.js";
+let time = 0;
 let messaggiHTML = "";
 conversazione.forEach((messaggio) => {
   messaggiHTML += `
   <div class="messaggio1">
           ${messaggio.messaggio1}
-          <span class="time-messaggio">12:38</span>
+          <span class="time-messaggio">12:3${time}</span>
         </div>
         
         <div class="messaggio2">
           ${messaggio.messaggio2}
-          <span class="time-messaggio">12:38</span>
+          <span class="time-messaggio">12:3${time}</span>
         </div>
-  `;
+  `
+  time++;
 });
+
+let nomeHeaderHTML = "";
+
 document.querySelector(".js-messaggi-utente").innerHTML = messaggiHTML;
 
 document.querySelector(".input").addEventListener("keypress", () => {
@@ -26,6 +30,18 @@ document.querySelector(".input").addEventListener("keypress", () => {
     messaggiHTML += nuovoMex;
     document.querySelector(".js-messaggi-utente").innerHTML = messaggiHTML;
     console.log(messaggio);
+    const numeroRandom = Math.floor(Math.random() * 4) + 1;
+    const rispostaMex = `
+    <div class="messaggio1">
+            ${risposte[numeroRandom]}
+            <span class="time-messaggio">12:38</span>
+          </div>`;
+    messaggiHTML += rispostaMex;
+    setTimeout(() => {
+      document.querySelector(".js-messaggi-utente").innerHTML = messaggiHTML;
+      scrollToBottom();
+    }, 2000);
+      
 
     document.querySelector(".input").value = "";
     scrollToBottom();
